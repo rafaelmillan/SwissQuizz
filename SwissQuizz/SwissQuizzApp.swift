6121cae15ec405a40ae812ef1e848c629b8c0879
Rafael Millán <rafmillan@gmail.com>
2023-12-04 18:45:19 +0100

    Initial Commit

---
+++ SwissQuizz/SwissQuizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwissQuizzApp.swift
+//  SwissQuizz
+//
+//  Created by Rafael Millan on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwissQuizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
