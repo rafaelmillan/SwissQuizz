126291a0edf9691f5bbc028faf2a7eea679c1006
Rafael Millán <rafmillan@gmail.com>
2024-03-25 10:59:47 +0100

    Make maps more difficult

--- SwissQuizz/Quizzes/Maps.swift
+++ SwissQuizz/Quizzes/Maps.swift
@@ -19,8 +19,8 @@ extension Quiz {
                 image: "maps/AG",
                 choices: [
                     Choice(text: String(localized: "Aargau"), isCorrect: true),
-                    Choice(text: String(localized: "Luzern")),
-                    Choice(text: String(localized: "Zug")),
+                    Choice(text: String(localized: "Solothurn")),
+                    Choice(text: String(localized: "Lucerne")),
                     Choice(text: String(localized: "Zürich"))
                 ]
             ),
@@ -29,19 +29,19 @@ extension Quiz {
                 image: "maps/AR",
                 choices: [
                     Choice(text: String(localized: "Appenzell Ausserrhoden"), isCorrect: true),
-                    Choice(text: String(localized: "Fribourg")),
-                    Choice(text: String(localized: "Basel-Stadt")),
-                    Choice(text: String(localized: "Schaffhausen"))
+                    Choice(text: String(localized: "Appenzell Innerrhoden")),
+                    Choice(text: String(localized: "Thurgau")),
+                    Choice(text: String(localized: "St. Gallen"))
                 ]
             ),
             Question(
                 prompt: String(localized: "Which canton is located here?"),
                 image: "maps/AI",
                 choices: [
-                    Choice(text: String(localized: "Fribourg")),
+                    Choice(text: String(localized: "Appenzell Ausserrhoden")),
                     Choice(text: String(localized: "Appenzell Innerrhoden"), isCorrect: true),
-                    Choice(text: String(localized: "Basel-Stadt")),
-                    Choice(text: String(localized: "Schaffhausen"))
+                    Choice(text: String(localized: "Thurgau")),
+                    Choice(text: String(localized: "St. Gallen"))
                 ]
             ),
             Question(
@@ -49,9 +49,9 @@ extension Quiz {
                 image: "maps/BL",
                 choices: [
                     Choice(text: String(localized: "Basel-Landschaft"), isCorrect: true),
-                    Choice(text: String(localized: "Jura")),
-                    Choice(text: String(localized: "Valais")),
-                    Choice(text: String(localized: "Obwalden"))
+                    Choice(text: String(localized: "Basel-Stadt")),
+                    Choice(text: String(localized: "Schaffhausen")),
+                    Choice(text: String(localized: "Solothurn"))
                 ]
             ),
             Question(
@@ -59,9 +59,9 @@ extension Quiz {
                 image: "maps/BS",
                 choices: [
                     Choice(text: String(localized: "Basel-Stadt"), isCorrect: true),
-                    Choice(text: String(localized: "Appenzell Innerrhoden")),
-                    Choice(text: String(localized: "Fribourg")),
-                    Choice(text: String(localized: "Appenzell Ausserrhoden"))
+                    Choice(text: String(localized: "Basel-Landschaft")),
+                    Choice(text: String(localized: "Schaffhausen")),
+                    Choice(text: String(localized: "Jura"))
                 ]
             ),
             Question(
@@ -69,9 +69,9 @@ extension Quiz {
                 image: "maps/BE",
                 choices: [
                     Choice(text: String(localized: "Bern"), isCorrect: true),
-                    Choice(text: String(localized: "Geneva")),
-                    Choice(text: String(localized: "Glarus")),
-                    Choice(text: String(localized: "Schaffhausen"))
+                    Choice(text: String(localized: "Fribourg")),
+                    Choice(text: String(localized: "Lucerne")),
+                    Choice(text: String(localized: "Valais"))
                 ]
             ),
             Question(
@@ -79,9 +79,9 @@ extension Quiz {
                 image: "maps/FR",
                 choices: [
                     Choice(text: String(localized: "Fribourg"), isCorrect: true),
-                    Choice(text: String(localized: "Zug")),
-                    Choice(text: String(localized: "Basel-Stadt")),
-                    Choice(text: String(localized: "Solothurn"))
+                    Choice(text: String(localized: "Bern")),
+                    Choice(text: String(localized: "Vaud")),
+                    Choice(text: String(localized: "Neuchâtel"))
                 ]
             ),
             Question(
@@ -89,9 +89,9 @@ extension Quiz {
                 image: "maps/GE",
                 choices: [
                     Choice(text: String(localized: "Geneva"), isCorrect: true),
-                    Choice(text: String(localized: "Glarus")),
-                    Choice(text: String(localized: "Bern")),
-                    Choice(text: String(localized: "Schaffhausen"))
+                    Choice(text: String(localized: "Vaud")),
+                    Choice(text: String(localized: "Fribourg")),
+                    Choice(text: String(localized: "Neuchâtel"))
                 ]
             ),
             Question(
@@ -99,9 +99,9 @@ extension Quiz {
                 image: "maps/GL",
                 choices: [
                     Choice(text: String(localized: "Glarus"), isCorrect: true),
+                    Choice(text: String(localized: "St. Gallen")),
                     Choice(text: String(localized: "Schwyz")),
-                    Choice(text: String(localized: "Bern")),
-                    Choice(text: String(localized: "Nidwalden"))
+                    Choice(text: String(localized: "Zug"))
                 ]
             ),
             Question(
@@ -109,9 +109,9 @@ extension Quiz {
                 image: "maps/GR",
                 choices: [
                     Choice(text: String(localized: "Grisons"), isCorrect: true),
-                    Choice(text: String(localized: "Thurgau")),
-                    Choice(text: String(localized: "Geneva")),
-                    Choice(text: String(localized: "Obwalden"))
+                    Choice(text: String(localized: "Ticino")),
+                    Choice(text: String(localized: "Glarus")),
+                    Choice(text: String(localized: "St. Gallen"))
                 ]
             ),
             Question(
@@ -119,9 +119,9 @@ extension Quiz {
                 image: "maps/JU",
                 choices: [
                     Choice(text: String(localized: "Jura"), isCorrect: true),
-                    Choice(text: String(localized: "Valais")),
+                    Choice(text: String(localized: "Neuchâtel")),
                     Choice(text: String(localized: "Basel-Landschaft")),
-                    Choice(text: String(localized: "Obwalden"))
+                    Choice(text: String(localized: "Solothurn"))
                 ]
             ),
             Question(
@@ -129,9 +129,9 @@ extension Quiz {
                 image: "maps/LU",
                 choices: [
                     Choice(text: String(localized: "Lucerne"), isCorrect: true),
-                    Choice(text: String(localized: "Zürich")),
-                    Choice(text: String(localized: "Zug")),
-                    Choice(text: String(localized: "Aargau"))
+                    Choice(text: String(localized: "Obwalden")),
+                    Choice(text: String(localized: "Nidwalden")),
+                    Choice(text: String(localized: "Uri"))
                 ]
             ),
             Question(
@@ -139,9 +139,9 @@ extension Quiz {
                 image: "maps/NE",
                 choices: [
                     Choice(text: String(localized: "Neuchâtel"), isCorrect: true),
-                    Choice(text: String(localized: "Ticino")),
+                    Choice(text: String(localized: "Jura")),
                     Choice(text: String(localized: "Vaud")),
-                    Choice(text: String(localized: "St. Gallen"))
+                    Choice(text: String(localized: "Basel-Landschaft"))
                 ]
             ),
             Question(
@@ -149,9 +149,9 @@ extension Quiz {
                 image: "maps/NW",
                 choices: [
                     Choice(text: String(localized: "Nidwalden"), isCorrect: true),
-                    Choice(text: String(localized: "Schwyz")),
-                    Choice(text: String(localized: "Solothurn")),
-                    Choice(text: String(localized: "Jura"))
+                    Choice(text: String(localized: "Obwalden")),
+                    Choice(text: String(localized: "Uri")),
+                    Choice(text: String(localized: "Schwyz"))
                 ]
             ),
             Question(
@@ -159,9 +159,9 @@ extension Quiz {
                 image: "maps/OW",
                 choices: [
                     Choice(text: String(localized: "Obwalden"), isCorrect: true),
-                    Choice(text: String(localized: "Valais")),
-                    Choice(text: String(localized: "Solothurn")),
-                    Choice(text: String(localized: "Jura"))
+                    Choice(text: String(localized: "Nidwalden")),
+                    Choice(text: String(localized: "Uri")),
+                    Choice(text: String(localized: "Lucerne"))
                 ]
             ),
             Question(
@@ -169,9 +169,9 @@ extension Quiz {
                 image: "maps/SH",
                 choices: [
                     Choice(text: String(localized: "Schaffhausen"), isCorrect: true),
-                    Choice(text: String(localized: "Uri")),
-                    Choice(text: String(localized: "Appenzell Innerrhoden")),
-                    Choice(text: String(localized: "Geneva"))
+                    Choice(text: String(localized: "Basel-Landschaft")),
+                    Choice(text: String(localized: "Thurgau")),
+                    Choice(text: String(localized: "Aargau"))
                 ]
             ),
             Question(
@@ -180,8 +180,8 @@ extension Quiz {
                 choices: [
                     Choice(text: String(localized: "Schwyz"), isCorrect: true),
                     Choice(text: String(localized: "Glarus")),
-                    Choice(text: String(localized: "Ticino")),
-                    Choice(text: String(localized: "Solothurn"))
+                    Choice(text: String(localized: "Zug")),
+                    Choice(text: String(localized: "Uri"))
                 ]
             ),
             Question(
@@ -189,9 +189,9 @@ extension Quiz {
                 image: "maps/SO",
                 choices: [
                     Choice(text: String(localized: "Solothurn"), isCorrect: true),
-                    Choice(text: String(localized: "Obwalden")),
-                    Choice(text: String(localized: "Valais")),
-                    Choice(text: String(localized: "Jura"))
+                    Choice(text: String(localized: "Aargau")),
+                    Choice(text: String(localized: "Zürich")),
+                    Choice(text: String(localized: "Schaffhausen"))
                 ]
             ),
             Question(
@@ -199,9 +199,9 @@ extension Quiz {
                 image: "maps/SG",
                 choices: [
                     Choice(text: String(localized: "St. Gallen"), isCorrect: true),
-                    Choice(text: String(localized: "Thurgau")),
-                    Choice(text: String(localized: "Vaud")),
-                    Choice(text: String(localized: "Neuchâtel"))
+                    Choice(text: String(localized: "Zürich")),
+                    Choice(text: String(localized: "Schwyz")),
+                    Choice(text: String(localized: "Glarus"))
                 ]
             ),
             Question(
@@ -209,8 +209,8 @@ extension Quiz {
                 image: "maps/TG",
                 choices: [
                     Choice(text: String(localized: "Thurgau"), isCorrect: true),
-                    Choice(text: String(localized: "St. Gallen")),
-                    Choice(text: String(localized: "Grisons")),
+                    Choice(text: String(localized: "Schaffhausen")),
+                    Choice(text: String(localized: "Aargau")),
                     Choice(text: String(localized: "Appenzell Innerrhoden"))
                 ]
             ),
@@ -219,9 +219,9 @@ extension Quiz {
                 image: "maps/TI",
                 choices: [
                     Choice(text: String(localized: "Ticino"), isCorrect: true),
-                    Choice(text: String(localized: "Neuchâtel")),
-                    Choice(text: String(localized: "Glarus")),
-                    Choice(text: String(localized: "Schwyz"))
+                    Choice(text: String(localized: "Valais")),
+                    Choice(text: String(localized: "Grisons")),
+                    Choice(text: String(localized: "Uri"))
                 ]
             ),
             Question(
@@ -229,9 +229,9 @@ extension Quiz {
                 image: "maps/UR",
                 choices: [
                     Choice(text: String(localized: "Uri"), isCorrect: true),
-                    Choice(text: String(localized: "Schaffhausen")),
-                    Choice(text: String(localized: "Bern")),
-                    Choice(text: String(localized: "Thurgau"))
+                    Choice(text: String(localized: "Obwalden")),
+                    Choice(text: String(localized: "Glarus")),
+                    Choice(text: String(localized: "Schwyz"))
                 ]
             ),
             Question(
@@ -239,9 +239,9 @@ extension Quiz {
                 image: "maps/VS",
                 choices: [
                     Choice(text: String(localized: "Valais"), isCorrect: true),
-                    Choice(text: String(localized: "Solothurn")),
-                    Choice(text: String(localized: "Obwalden")),
-                    Choice(text: String(localized: "Jura"))
+                    Choice(text: String(localized: "Bern")),
+                    Choice(text: String(localized: "Vaud")),
+                    Choice(text: String(localized: "Ticino"))
                 ]
             ),
             Question(
@@ -249,8 +249,8 @@ extension Quiz {
                 image: "maps/VD",
                 choices: [
                     Choice(text: String(localized: "Vaud"), isCorrect: true),
-                    Choice(text: String(localized: "Thurgau")),
-                    Choice(text: String(localized: "St. Gallen")),
+                    Choice(text: String(localized: "Fribourg")),
+                    Choice(text: String(localized: "Bern")),
                     Choice(text: String(localized: "Geneva"))
                 ]
             ),
@@ -259,9 +259,9 @@ extension Quiz {
                 image: "maps/ZG",
                 choices: [
                     Choice(text: String(localized: "Zug"), isCorrect: true),
-                    Choice(text: String(localized: "Zürich")),
+                    Choice(text: String(localized: "Schwyz")),
                     Choice(text: String(localized: "Lucerne")),
-                    Choice(text: String(localized: "Aargau"))
+                    Choice(text: String(localized: "Nidwalden"))
                 ]
             ),
             Question(
@@ -269,8 +269,8 @@ extension Quiz {
                 image: "maps/ZH",
                 choices: [
                     Choice(text: String(localized: "Zürich"), isCorrect: true),
-                    Choice(text: String(localized: "Zug")),
-                    Choice(text: String(localized: "Lucerne")),
+                    Choice(text: String(localized: "St. Gallen")),
+                    Choice(text: String(localized: "Thurgau")),
                     Choice(text: String(localized: "Aargau"))
                 ]
             )
